679caebb9d9f37b6485efb5b48554abfaead82f7
sagivStekolshik <sagivste@gmail.com>
2019-09-20 01:45:26 +0300

    start writeing test for matrix basic func

---
+++ __test__/basic.test.js
@@ -0,0 +1,18 @@
+const createMatrix = () => ({})
+describe('checking the matrix', () => {
+    
+    
+    beforeEach(() => {
+        this.matrix = createMatrix()
+    })
+
+    test('new matrix is empty', () => {
+        expect(this.matrix).toStrictEqual({})
+    })
+
+    test('can place random points', () => {
+        
+    })
+
+
+})
